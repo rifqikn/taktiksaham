3c1f78af377272430962240545213f887ac4779b
rifqikn <103616350+rifqikn@users.noreply.github.com>
2026-08-24 12:23:43 +0700

    Add files via upload

---
+++ financial-config.js
@@ -0,0 +1,5 @@
+window.TAKTIK_FINANCIAL_CONFIG = {
+  supabaseUrl: "https://hjwykqopradagpjrtyif.supabase.co",
+  publishableKey: "sb_publishable_zGrDTMqkZhQlgz9Oj5RPSw_6rvhp7wf",
+  functionName: "eodhd-financials"
+};
