82608fafa63e94a6c88e93b0f1b9036c35893f8d
rifqikn <103616350+rifqikn@users.noreply.github.com>
2026-08-24 12:22:32 +0700

    Delete financial-config.js

--- financial-config.js
+++
@@ -1,5 +0,0 @@
-window.TAKTIK_FINANCIAL_CONFIG = {
-  supabaseUrl: "https://hjwykqopradagpjrtyif.supabase.co",
-  publishableKey: "sb_publishable_zGrDTMqkZhQlgz9Oj5RPSw_6rvhp7wf",
-  functionName: "eodhd-financials"
-};
